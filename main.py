d73393d8bc9c56a1e2f6e1cc3286d819a53a8563
dong73097@gmail.com <dong73097@gmail.com>
2023-04-01 00:25:21 +0900

    create DB.txt and update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import serial
-import sqlite3
+import pymysql
 
 ser = serial.Serial("COM3", 115200, timeout = 1)
+con = pymysql.connect(host='localhost', user='mysql_user_id', password='_password_', db='access_db', charset='utf8')
+
+cur = con.cursor()
+
+data = None
+order = None
+sqlinsdata = "INSERT INTO A VALUES(NOW(), " + order + ", " + data + ");"
 while True:
-    print(ser.readline())
+    if ser.readable():
+        orgdata = ser.readline()
+        
