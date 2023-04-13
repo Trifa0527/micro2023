153f2e67670ea466ba7a6ee03c4344ee81f7eca9
dong73097@gmail.com <dong73097@gmail.com>
2023-04-13 23:15:45 +0900

    preloading

--- micro/mainapp/static/update.js
+++ micro/mainapp/static/update.js
@@ -20,4 +20,15 @@ function ajaxcall(){
         }
     });
 }
+function preloading (imageArray) {
+    let n = imageArray.length;
+    for (let i = 0; i < n; i++) {
+        let img = new Image();
+        img.src = imageArray[i];
+    }
+}
+preloading([
+    'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202104/20/ab7c9cda-2fca-4205-8d10-540f5851823f.jpg',
+    'https://i.ytimg.com/vi/zQ9Po9UDqBc/maxresdefault.jpg'
+])
 setInterval(ajaxcall, 1000);
