38afbaae6ca85cb99d2bad3bd8deb40145bd4762
yys <dong73097@gmail.com>
2023-04-14 16:04:07 +0900

    upload complete

--- microbit.js
+++ microbit.js
@@ -10,8 +10,8 @@ BaudRate.BaudRate115200
 basic.forever(function () {
     ult = randint(0, 10)
     tem = randint(0, 10)
-    gyro_x = randint(0, 10)
-    gyro_y = randint(0, 10)
+    gyro_x = input.acceleration(Dimension.X)
+    gyro_y = input.acceleration(Dimension.Y)
     serial.writeString("" + ult + "%%" + tem + "%%" + gyro_x + "." + gyro_y)
     basic.pause(1000)
 })
