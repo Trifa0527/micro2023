a88ae772e44c67beaf6112b97f5f553958f23edd
yys <dong73097@gmail.com>
2023-04-14 15:53:34 +0900

    upload complete

--- micro/staticfiles/update.js
+++ micro/staticfiles/update.js
@@ -11,6 +11,8 @@ function ajaxcall(){
             document.getElementById('tem').innerHTML=t+data['tem'] + '°C';
             document.getElementById('gyro').innerHTML=g+ 'x: '+ data['gyro_x'] + '  y:' + data['gyro_y'] +'°';
             document.getElementById('ti').innerHTML=data['time']
+            document.getElementById('img').setAttribute('src', 'https://image.newsis.com/2021/11/01/NISI20211101_0000859131_web.jpg?rnd=20211101115531');
+
             if(Math.floor()*100>50){
                 document.getElementById('title').innerHTML='Elon';
                 document.getElementById('img').setAttribute('src', 'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202104/20/ab7c9cda-2fca-4205-8d10-540f5851823f.jpg');
@@ -28,7 +30,7 @@ function ajaxcall(){
                         document.getElementById('tem').innerHTML=t+data['tem'] + '°C';
                         document.getElementById('gyro').innerHTML=g+ 'x: '+ data['gyro_x'] + '  y:' + data['gyro_y'] +'°';
                         document.getElementById('ti').innerHTML=data['time']
-                        if(a>50){
+                        if(ran>50){
                             document.getElementById('title').innerHTML='Elon';
                             document.getElementById('img').setAttribute('src', 'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202104/20/ab7c9cda-2fca-4205-8d10-540f5851823f.jpg');
                         }else{
@@ -45,10 +47,6 @@ function ajaxcall(){
                     img.src = imageArray[i];
                 }
             }
-            preloading([
-                'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202104/20/ab7c9cda-2fca-4205-8d10-540f5851823f.jpg',
-                'https://i.ytimg.com/vi/zQ9Po9UDqBc/maxresdefault.jpg'
-            ])
             setInterval(ajaxcall, 1000);
                 document.getElementById('title').innerHTML='Jeffrey';
                 document.getElementById('img').setAttribute('src', 'https://i.ytimg.com/vi/zQ9Po9UDqBc/maxresdefault.jpg');
@@ -65,6 +63,7 @@ function preloading (imageArray) {
 }
 preloading([
     'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202104/20/ab7c9cda-2fca-4205-8d10-540f5851823f.jpg',
-    'https://i.ytimg.com/vi/zQ9Po9UDqBc/maxresdefault.jpg'
+    'https://i.ytimg.com/vi/zQ9Po9UDqBc/maxresdefault.jpg',
+    'https://image.newsis.com/2021/11/01/NISI20211101_0000859131_web.jpg?rnd=20211101115531'
 ])
 setInterval(ajaxcall, 1000);
